af3e8091f8f4450f70dd99b88582ffc4c80c921f
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-04-09 19:55:21 +0530

    ffg

--- product/index.php
+++ product/index.php
@@ -1,6 +1,8 @@
 <?php 
 
+
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
