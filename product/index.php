1944898210455e5f6bc95d5c72de90202b43273a
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-04-09 19:52:11 +0530

    ffg

--- product/index.php
+++ product/index.php
@@ -1,6 +1,9 @@
 <?php 
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
     $a = ['Eggs','Milk','NewsPapper'];
     echo json_encode($a)
 ?>
